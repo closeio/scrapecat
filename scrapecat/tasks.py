2c3740daf4590ed759e49493e93392ab01e9d908
Anthony Nemitz <anemitz@gmail.com>
2012-05-10 15:08:48 -0700

    ghost used in task.  kills the worker thread.  wip

--- scrapecat/tasks.py
+++ scrapecat/tasks.py
@@ -1,3 +1,5 @@
+import datetime
+import ghost
 import requests
 from scrapecat import app, models
 from flask.ext.celery import Celery
@@ -8,14 +10,31 @@ celery = Celery(app)
 @celery.task(name="scrapecat.scraper.scrape")
 def scrape(url, postback_url=None):
     success = False
-    response = requests.get(url)     
-    if response.status_code == 200:
-        webpage = models.Webpage(url=response.url, headers=response.headers, html=response.text)
-        webpage.save()
-        
-        #process the plugins
+    webpage = None
+    try:
+        webpage = models.Webpage.objects.get(url=url)
+    except models.Webpage.DoesNotExist:
+        print "no cached page"
 
-        success = True
+    if not webpage or (webpage and webpage.date_created < (datetime.datetime.now() - datetime.timedelta(days=1))):
+        print "refresh a cached page"
+        g = ghost.Ghost(wait_timeout=60)
+        print "start ghost open url"
+        page, resources = g.open(url)
+        print "done opening url in ghost"
+        if response.status_code != 200 and not webpage:
+            print "status != 200 and no previously cached page"
+            success = False    
+        if response.status_code == 200:
+            print "status == 200 and saving webpage"
+            webpage = models.Webpage(url=page.url, headers=page.headers, html=ghost.main_frame.toHtml())
+            webpage.save()
+    else:
+        print "using cached page!"
+    
+    #process the plugins
+
+    success = True
     if postback_url:
         response = requests.post(postback_url, data=webpage if success else {"error": "your shit is fucked up, bitch!"})
     return success
