d7704ca37b45ea6b7e0c7b655f67bd456f991c84
Thomas Steinacher <tom@eggdrop.ch>
2012-05-09 19:34:44 -0700

    Fix indentation

--- scrapecat/plugins.py
+++ scrapecat/plugins.py
@@ -1,6 +1,8 @@
 import sys
 import logging
 import ghost
+from scrapecat import models
+from PySide.QtWebKit import QWebSettings
 
 import phonenumbers
 
@@ -10,13 +12,16 @@ class Plugin(object):
         self.url = kwargs['url']
         self.ghost = ghost.Ghost(wait_timeout=60)
         logging.debug('Created Ghost object')
+
+        QWebSettings.globalSettings().setAttribute(QWebSettings.AutoLoadImages, False)
+        QWebSettings.globalSettings().setAttribute(QWebSettings.PluginsEnabled, False)
         self.page, self.resources = self.ghost.open(self.url)
         logging.debug('Fetched the page')
 
     def process(self, webpage):
         raise NotImplemented
 
-    def traverse(self, parent_node, match_el=None, match_text=None, depth=0):
+    def traverse(self, parent_node, match_el=None, match_text=None, depth=0, ignore_tags=None):
         ret = []
         node_info = parent_node.evaluateJavaScript(
                 '''(function(el) {
@@ -26,24 +31,30 @@ class Plugin(object):
                         el = el.nextSibling;
                     } return ret;
                 })(this);''')
+
+        if ignore_tags is None:
+            ignore_tags = ['SCRIPT', 'NOSCRIPT']
+
         if match_el and match_el(parent_node):
             ret.append(parent_node)
         if node_info:
             node = parent_node.firstChild()
             for node_type, node_value in node_info:
-                  if node_type == 1: # element node
-                      ret += self.traverse(node,
-                              match_el=match_el,
-                              match_text=match_text,
-                              depth=depth+1)
-                      # print ' '*depth, 'NODE', node.tagName()
-                      node = node.nextSibling()
-                  elif node_type == 3: # text node
-                      stripped_node_value = node_value.strip()
-                      if stripped_node_value: # skip empty nodes
-                              if match_text and match_text(stripped_node_value):
-                                  ret.append(parent_node)
-                              # print ' '*depth, 'TEXT', stripped_node_value
+                if node_type == 1: # element node
+                    if node.tagName() not in ignore_tags:
+
+                        ret += self.traverse(node,
+                            match_el=match_el,
+                            match_text=match_text,
+                            depth=depth+1)
+                        # print ' '*depth, 'NODE', node.tagName()
+                        node = node.nextSibling()
+                elif node_type == 3: # text node
+                    stripped_node_value = node_value.strip()
+                    if stripped_node_value: # skip empty nodes
+                          if match_text and match_text(stripped_node_value):
+                              ret.append(parent_node)
+                          # print ' '*depth, 'TEXT', stripped_node_value
         return ret
 
 class BasePlugin(Plugin):
