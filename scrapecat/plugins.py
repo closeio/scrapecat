a64294bae7366df24f928c4e9bd6785aa47ad577
Thomas Steinacher <tom@eggdrop.ch>
2012-05-09 21:20:44 -0700

    Magic to match contacts and phones.

--- scrapecat/plugins.py
+++ scrapecat/plugins.py
@@ -41,22 +41,30 @@ class BasePlugin(Plugin):
 
 class ContactPlugin(Plugin):
     def process(self):
+        emails = self.emails()
+        phones = self.phones()
+        contacts = self.contacts()
+
         return {
-            'emails' : self.emails(),
-            'phone_numbers' : self.phone_numbers()
+            'emails' : emails,
+            'phones' : phones,
+            'contacts': contacts,
         }
 
     def emails(self):
-        els = utils.traverse(self.body,
+        self.email_els = utils.traverse(self.body,
                 match_el=lambda el: utils.find_emails(el.attribute('href')),
                 match_text=lambda s: utils.find_emails(s), ignore_tags=[])
-        return list(chain(*[utils.find_emails(el.attribute('href')) + utils.find_emails(el.toPlainText()) for el in els]))
+        return list(chain(*[utils.find_emails(el.attribute('href')) + utils.find_emails(el.toPlainText()) for el in self.email_els]))
 
-    def phone_numbers(self):
+    def phones(self):
         number_match = lambda t: list(phonenumbers.PhoneNumberMatcher(t, 'US'))
-        els = utils.traverse(self.body,
+        self.phone_els = utils.traverse(self.body,
                 match_text=number_match)
-        return list(chain(*[[match.raw_string for match in phonenumbers.PhoneNumberMatcher(el.toPlainText(), 'US')] for el in els]))
+        return list(chain(*[[match.raw_string for match in phonenumbers.PhoneNumberMatcher(el.toPlainText(), 'US')] for el in self.phone_els]))
+
+    def contacts(self):
+        return [[email.attribute('href'), phone.toPlainText()] for email, phone, group_parent in utils.group_by_common_parents(self.email_els, self.phone_els)]
 
 if __name__ == '__main__':
     url = sys.argv[1]
