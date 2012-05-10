d9966cd437fcc2f188db4b18be64d1572b2eb84a
Anthony Nemitz <anemitz@gmail.com>
2012-05-10 11:45:26 -0700

    syntactic sugar

--- scrapecat/tasks.py
+++ scrapecat/tasks.py
@@ -7,11 +7,14 @@ celery = Celery(app)
 
 @celery.task(name="scrapecat.scraper.scrape")
 def scrape(url, postback_url=None):
-    response = requests.get(url)     
     success = False
+    response = requests.get(url)     
     if response.status_code == 200:
         webpage = models.Webpage(url=response.url, headers=response.headers, html=response.text)
         webpage.save()
+        
+        #process the plugins
+
         success = True
     if postback_url:
         response = requests.post(postback_url, data=webpage if success else {"error": "your shit is fucked up, bitch!"})
