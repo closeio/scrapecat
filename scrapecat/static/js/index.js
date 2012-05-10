129bc430535e3ffc2ea95f52f61f19c6bc7e5760
Patrick O'Doherty <p@trickod.com>
2012-05-10 15:19:36 -0700

    Make the homepage use iCanHasJS templates instead of jQuery hackery

--- scrapecat/static/js/index.js
+++ scrapecat/static/js/index.js
@@ -7,18 +7,27 @@ $('#ScrapeRequestForm').ajaxForm({
     success: function(results) {
         function renderContacts(contacts) {
             for (var i = 0; i < contacts.length; i++){
-                $('table#contacts').append("<tr><td>" + contacts[i].phones +"</td><td>" + contacts[i].emails + "</td></tr>");
+                var contact = ich.contact(contacts[i]);
+                $('table#contacts').append(contact);
             }
         };
+        function renderElement(type, list) {
+            type_singular = type.substring(0, type.length - 1);
+            for (var i = 0; i < list.length; i++) {
+                var d = {};
+                d[type_singular] = list[i];
+                var el = ich[type_singular](d);
+                $('#ScrapeRequestFormResults .' + type + ' ul').append(el);
+            }
+        }
         $('form#ScrapeRequestForm button#submit').addClass('btn-primary').removeClass('disabled');
         for (var type in results) {
             if (results.hasOwnProperty(type)){
                 values = results[type]
                 if (type == "contacts")
                     renderContacts(values)
-                else
-                    for (var i = 0; i < values.length; i ++)
-                        $('#ScrapeRequestFormResults .' + type + ' ul').append("<li>" + values[i] + "</li>");
+                else if (type == "phones" || type == "emails")
+                    renderElement(type, values);
             }
         }
         $('#ScrapeRequestFormResults').show(500);
